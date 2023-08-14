921f03c2b8c0f667617fddd49b21c9f7b9915b17
Zain Ul Abideen <zain.malik@paysky.io>
2023-08-14 15:15:46 +0500

    code cleaning

--- routes/auth.js
+++ routes/auth.js
@@ -5,10 +5,6 @@ const authController = require('../controllers/auth');
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin);
-
-router.get('/signup', authController.getSignup);
-
 router.post('/login',
 [
     body('email')
@@ -48,13 +44,5 @@ check('email').isEmail().withMessage("Please enter a valid email.").custom((valu
    authController.signup);
 
 router.post('/logout', authController.postLogout);
-
-router.get('/reset', authController.getReset);
-
-router.post('/reset', authController.postReset);
-
-router.get('/reset/:token', authController.getNewPassword);
-
-router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
